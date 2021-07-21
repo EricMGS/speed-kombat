99437660fcf7434e36298efcfb6f878746e65718
Devon <devonwayman97@gmail.com>
2021-07-21 13:06:41 -0400

    Added English language support

--- speed-kombat.py
+++ speed-kombat.py
@@ -4,11 +4,12 @@
 #Date: april 2018
 
 #Imports
-from tkinter import *
-from tkinter import messagebox
+from tkinter  import *
+from tkinter  import messagebox
 import time
 import sys
 from random import *
+from os import system, name
 
 #Variables
 p1_x = 683
@@ -19,8 +20,9 @@ square_x = 0
 square_y = 0
 q_times = 20
 
-#Functions
+user_langage = ""
 
+#Functions
 def square_rnd ():
     global square_x
     global square_y
@@ -39,9 +41,18 @@ def verify():
         square_rnd()
         speed += 1.5
         q_times -= 1
-        times.configure(text = (q_times, 'restantes'))
+
+        if user_langage == 'port':
+            times.configure(text = (q_times, 'restantes'))
+        elif user_langage == 'en':
+            times.configure(text = (q_times, 'remaining'))
+        
         if q_times == 0:
-            messagebox.showinfo ('Fim', 'Você ganhou')
+            if user_langage == 'port':
+                messagebox.showinfo ('Fim', 'Você ganhou')
+            elif user_langage == 'en':
+                messagebox.showinfo ('End', 'You won')
+            
             sys.exit ()
 
 def p1_right (event):
@@ -109,6 +120,7 @@ def p1_down (event):
     global p1_function
     global speed
     p1_function = 4
+
     while p1_y < 663:
         if p1_function == 4:
             verify()
@@ -124,7 +136,28 @@ def p1_down (event):
             return
     return
 
-#Window Configurations
+def clear_screen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def exit_game(event):
+    sys.exit()
+
+
+clear_screen()
+
+user_input = input("Please select a language | Selecione um idioma\n 1) English | inglês\n 2) Portugese | português\n --> ")
+
+if user_input == '1' or user_input.lower() == 'english' or user_input.lower == 'inglês':
+    user_langage = "en"
+elif user_input == '2' or user_input.lower() == 'Portugese' or user_input.lower == 'português':
+    user_langage = "port"
+
+clear_screen()
+
+# Window Configurations
 window = Tk ()
 window.attributes ('-fullscreen', True)
 window.title ('Speed Kombat')
@@ -140,10 +173,18 @@ division = Frame (width = 1366, height = 100, bg = 'White')
 division.place (relx = 0.5, rely = 1, anchor = S)
 
 #Labels
-instruction = Label (text = 'Você é o quadrado azul\nVocê deve pegar o quadrado preto 20 vezes\nUse as setas direcionais', bg = 'white', font = ('arial bold', 15))
+if user_langage == 'port':
+    instruction = Label (text = 'Você é o quadrado azul\nVocê deve pegar o quadrado preto 20 vezes\nUse as setas direcionais\nPressione Escape para sair do jogo', bg = 'white', font = ('arial bold', 15))
+elif user_langage == 'en':
+    instruction = Label (text = 'You are the blue square\nYou must get the black square 20 times\nUse the directional arrows\nPress Escape to exit game', bg = 'white', font = ('arial bold', 15))
+
 instruction.place (relx = 0.3, rely = 0.93, anchor = CENTER)
 
-times = Label (text = (q_times, 'restantes'), font = ('impact', 20), bg = 'white')
+if user_langage == 'port':
+    times = Label (text = (q_times, 'restantes'), font = ('impact', 20), bg = 'white')
+elif user_langage == 'en':
+    times = Label (text = (q_times, 'remaining'), font = ('impact', 20), bg = 'white')
+
 times.place (relx = 0.8, rely = 0.93, anchor = CENTER)
 
 #Binds
@@ -151,7 +192,7 @@ window.bind ('<Right>', p1_right)
 window.bind ('<Left>', p1_left)
 window.bind ('<Up>', p1_up)
 window.bind ('<Down>', p1_down)
-
+window.bind('<Escape>', exit_game)
 
 square_rnd ()
 
